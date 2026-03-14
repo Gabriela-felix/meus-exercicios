5b47b7acfd0374f07184d4677414e95a8904812a
Gabriela-felix <gabriellafelix_@hotmail.com>
2026-03-14 12:33:37 +0000

    estudando

---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+#Criar um script python que leia o dia, mês e o ano de nascimento
+# da pessoa e mostre uma mensagem com a data.
+
+dia = input ('Digite o dia que você nasceu:')
+mes = input ('Agora digite o mês:')
+ano = input ('E agora digite o ano:')
+print ('Você nasceu no dia',dia,'de',mes,'no ano',ano,',certo?')
