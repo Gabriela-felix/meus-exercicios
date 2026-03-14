a931a26603a99782565764574ec1ccd1804f0174
Gabriela-felix <gabriellafelix_@hotmail.com>
2026-03-14 12:43:31 +0000

    soma de dois números

---
+++ exercicio03.py
@@ -0,0 +1,6 @@
+#Crie um ecript python que leia dois números e mostre
+# a soma entre eles.
+num1 = input('Digite o primeiro número:')
+num2 = input ('Digite o segundo número:')
+soma = int (num1) + int (num2)
+print ('A soma dos dois números é:',soma)
