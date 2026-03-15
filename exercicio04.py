c040c9f41cae8c9cba33aeb2332b039bcfcf0049
Gabriela-felix <gabriellafelix_@hotmail.com>
2026-03-15 17:46:20 -0300

    multiplicando números

---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+#crie um script em python que leia dois números, multiplique e mostre n\
+# o resultado.
+num1 = input ('Digite o primeiro número:')
+num2 = input ('Digite o segundo número:')
+multiplicacao= int (num1) * int (num2)
+print ('A multiplicação dos dois números é:',multiplicacao)
